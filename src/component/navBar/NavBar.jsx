e47563045c2afec4d6ecf10c5137d3cf1f154306
2233405 <2233405@collegemv.qc.ca>
2025-03-07 14:52:01 -0500

    Ajouter navbar

---
+++ src/component/navBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import 'react'
+import './NavBar.css';
+
+export default function NavBar() {
+    return(
+        <div className='nav-grid'>
+            <span class="material-symbols-outlined">
+                home
+            </span>
+            <span class="material-symbols-outlined">
+                home
+            </span>
+            <span class="material-symbols-outlined">
+                home
+            </span>
+            <span class="material-symbols-outlined">
+                home
+            </span>
+            <span class="material-symbols-outlined">
+                home
+            </span>
+            <span class="material-symbols-outlined">
+                home
+            </span>
+        </div>
+    );
+}
