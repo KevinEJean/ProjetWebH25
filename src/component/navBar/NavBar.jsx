b376816cd9d6c2427f007a88f5ae7b56d4784f4a
2233405 <my@mail.com>
2025-03-31 15:03:25 -0400

    little fix

--- src/component/navBar/NavBar.jsx
+++ src/component/navBar/NavBar.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function NavBar() {
-    const [show, setShow] = useState();
     return (
         <div className='nav-grid'>
             <nav>
--- src/component/settings/Setting.jsx
+++ src/component/settings/Setting.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-// import $ from jquery;
 import './Setting.css';
 
 export default function Setting() {
 
     function colorPicker(thisColor) { // to be continued
-        var allH1Titles = document.getElementsByTagName("h1");
-        // var allH2Titles = ;
-        // var allH3Titles = ;
 
-        for (let i = 0; i < allH1Titles.length; i++) {
-            allH1Titles[i].style.color == thisColor;
-        }
-
-        // for (let i = 0; i < allH2Titles.length; i++) {
-        //     allH2Titles[i].style.color == thisColor;
-        //     console.log("H2 : " + allH2Titles[i].textContent);
+        // SETTINGS PAGE
+        localStorage.setItem("Title-Colors", thisColor);
+        // if (localStorage.getItem("Title-Colors")) {
+        //     var allH1Titles = document.getElementsByTagName("h1");
+        //     for (let i = 0; i < allH1Titles.length; i++) {
+        //         allH1Titles[i].style.color = thisColor;
+        //     }
         // }
 
-        // for (let i = 0; i < allH3Titles.length; i++) {
-        //     allH3Titles[i].style.color == thisColor;
-        //     console.log("H3 : " + allH3Titles[i].textContent);
-        // }
+
+        // HOME PAGE
+
+
+        // MOVIE PAGE
+
+
+        // DETAIL_MOVIE PAGE
+
+
+        // PROFIL PAGE
     }
 
     function copy() {
@@ -35,40 +38,40 @@ export default function Setting() {
         alert('Data succesfully erased !')
     }
 
-    return(
+    return (
         <div className="setting">
             <div className="theme-group">
                 <h1>Theme Preferences</h1>
                 <div className="theme-grid">
-                    <button onClick={(event) => colorPicker("lightgrey")} style={{color: "lightgrey"}}>default</button>
-                    <button onClick={(event) => colorPicker("red")} style={{color: "red"}}>red</button>
-                    <button onClick={(event) => colorPicker("cornflowerblue")} style={{color: "cornflowerblue"}}>blue</button>
-                    <button onClick={(event) => colorPicker("chartreuse")} style={{color: "chartreuse"}}>green</button>
-                    <button onClick={(event) => colorPicker("yellow")} style={{color: "yellow"}}>yellow</button>
-                    <button onClick={(event) => colorPicker("orangered")} style={{color: "orangered"}}>orange</button>
-                    <button onClick={(event) => colorPicker("blueviolet")} style={{color: "blueviolet"}}>purple</button>
-                    <button onClick={(event) => colorPicker("cyan")} style={{color: "cyan"}}>cyan</button>
-                    <button onClick={(event) => colorPicker("hotpink")} style={{color: "hotpink"}}>pink</button>
-                    <button onClick={(event) => colorPicker("teal")} style={{color: "teal"}}>teal</button>
-                    <button onClick={(event) => colorPicker("chocolate")} style={{color: "chocolate"}}>brown</button>
+                    <button onClick={(event) => colorPicker("lightgrey")} style={{ color: "lightgrey" }}>default</button>
+                    <button onClick={(event) => colorPicker("red")} style={{ color: "red" }}>red</button>
+                    <button onClick={(event) => colorPicker("cornflowerblue")} style={{ color: "cornflowerblue" }}>blue</button>
+                    <button onClick={(event) => colorPicker("chartreuse")} style={{ color: "chartreuse" }}>green</button>
+                    <button onClick={(event) => colorPicker("yellow")} style={{ color: "yellow" }}>yellow</button>
+                    <button onClick={(event) => colorPicker("orangered")} style={{ color: "orangered" }}>orange</button>
+                    <button onClick={(event) => colorPicker("blueviolet")} style={{ color: "blueviolet" }}>purple</button>
+                    <button onClick={(event) => colorPicker("cyan")} style={{ color: "cyan" }}>cyan</button>
+                    <button onClick={(event) => colorPicker("hotpink")} style={{ color: "hotpink" }}>pink</button>
+                    <button onClick={(event) => colorPicker("teal")} style={{ color: "teal" }}>teal</button>
+                    <button onClick={(event) => colorPicker("chocolate")} style={{ color: "chocolate" }}>brown</button>
                 </div>
             </div>
             <div className="data-group">
                 <h1>Data</h1>
-                <div style={{display: "flex"}}>
+                <div style={{ display: "flex" }}>
                     <p>This will erase all your data including saved films and series (Non-Reversable)</p>
-                    <button onClick={(event) => dataReset()} style={{marginLeft: "10px", color: "red"}}>Reset</button>
+                    <button onClick={(event) => dataReset()} style={{ marginLeft: "10px", color: "red" }}>Reset</button>
                 </div>
-                <div style={{display: "flex"}}>
+                <div style={{ display: "flex" }}>
                     <p>This will permanatly erase your account (Non-Reversable)</p>
-                    <Link to="/userDelete"><button style={{marginLeft: "10px", color: "red"}}>Delete</button></Link>
+                    <Link to="/userDelete"><button style={{ marginLeft: "10px", color: "red" }}>Delete</button></Link>
                 </div>
             </div>
             <div className="contact-group">
                 <h1>Information</h1>
-                <div style={{display: "flex", marginBottom: "50px"}}>
+                <div style={{ display: "flex", marginBottom: "50px" }}>
                     <p>Contact Us ?</p>
-                    <button onClick={(event) => copy()} style={{marginLeft: "10px", color: "lightgrey"}}>filmApp@gmail.com</button>
+                    <button onClick={(event) => copy()} style={{ marginLeft: "10px", color: "lightgrey" }}>filmApp@gmail.com</button>
                 </div>
             </div>
         </div>
