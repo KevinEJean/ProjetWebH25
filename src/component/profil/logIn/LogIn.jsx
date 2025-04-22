087649525fbbf5f4150bf4a45643246add7a1e72
Pape Momath Diba <6242577@collegemv.qc.ca>
2025-04-22 09:42:42 -0400

    corrige de l'erreur de ton import

--- src/component/profil/logIn/LogIn.jsx
+++ src/component/profil/logIn/LogIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import { submitRules } from '../UseConnection';
+import submitRules from '../UseConnection';
 import './LogIn.css';
 
 export default function LogIn() {
