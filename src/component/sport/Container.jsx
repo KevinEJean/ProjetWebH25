c2ba0ccd1ae9ab2728bfd4c7c98d82933b22f485
Kevin <2233405@collegemv.qc.ca>
2025-04-29 19:30:53 -0400

    page de  soccer

--- src/component/sport/Container.jsx
+++ src/component/sport/Container.jsx
@@ -1,25 +1,80 @@
-export default function Container({ className = "", corners = "10px", style = {}, onClick }) {
+import axios from "axios";
+import "./Container.css";
+import { useState } from "react";
 
-    fetch("https://v3.football.api-sports.io/fixtures?live=all", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "v3.football.api-sports.io",
-            "x-rapidapi-key": "cd4162348bcc498480bdbef575c867c4"
+export default function Container({ className = "" }) {
+
+    const [awayTeamName, setAwayTeamName] = useState("N/A");
+    const [awayTeamScore, setAwayTeamScore] = useState("N/A");
+    const [awayTeamCrest, setAwayTeamCrest] = useState();
+
+    const [matchStage, setMatchStage] = useState("This may take some time...");
+    const [matchStatus, setMatchStatus] = useState("Please be patient...");
+
+    const [homeTeamName, setHomeTeamName] = useState("N/A");
+    const [homeTeamScore, setHomeTeamScore] = useState("N/A");
+    const [homeTeamCrest, setHomeTeamCrest] = useState();
+
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = String(date.getMonth() + 1).padStart(2, '0');
+    var day = String(date.getDate()).padStart(2, '0');
+    var today = `${year}-${month}-${day}`;
+
+    axios({
+        method: "get",
+        headers: { "X-Auth-Token": "93d6121a8813417785020f8bc7dfcdfa" },
+        url: `https://api.football-data.org/v4/competitions/CL/matches`,
+        responseType: "json"
+    }).then(function (response) {
+        for (let i = 0; i < 190; i++) {
+            if (response.data.matches[i].utcDate.includes(today) && response.data.matches[i].awayTeam.shortName != undefined) {
+                console.log(response.data.matches[i]); // test
+
+                setAwayTeamName(response.data.matches[i].awayTeam.tla);
+                setHomeTeamName(response.data.matches[i].homeTeam.tla);
+
+                setAwayTeamCrest(response.data.matches[i].awayTeam.crest);
+                setHomeTeamCrest(response.data.matches[i].homeTeam.crest);
+
+                setMatchStatus(response.data.matches[i].status);
+                setMatchStage(response.data.matches[i].stage);
+
+                setAwayTeamScore(response.data.matches[i].score.fullTime.away);
+                setHomeTeamScore(response.data.matches[i].score.fullTime.home);
+                
+                if (awayTeamScore > homeTeamScore) {
+                    document.getElementById("awayTeamScore").style.textDecoration = "underline";
+                } else if (awayTeamScore < homeTeamScore) {
+                    document.getElementById("homeTeamScore").style.textDecoration = "underline";
+                }
+            }
         }
     })
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-    
 
-    
+    if (className == "fifa") {
+        return (
+            <div className="fifa-container-root">
+                <div className="fifa-container">
+                    <h2 style={{ color: localStorage.getItem("Title-Colors") }}>{awayTeamName}</h2>
+                    <p>{matchStage}</p>
+                    <h2 style={{ color: localStorage.getItem("Title-Colors") }}>{homeTeamName}</h2>
 
-    return (
-        <div>
-            <h1>{}</h1>
-        </div>
-    )
+                    <img alt={awayTeamName} src={awayTeamCrest} />
+                    <h2>VS</h2>
+                    <img alt={homeTeamName} src={homeTeamCrest} />
+
+                    <h2 id="awayTeamScore">{awayTeamScore}</h2>
+                    <p style={{color: "yellow"}}>{matchStatus}</p>
+                    <h2 id="homeTeamScore">{homeTeamScore}</h2>
+                </div>
+            </div>
+        );
+    } else if (className == "nba") {
+        return (
+            <div>
+                <h1>NBA</h1>
+            </div>
+        );
+    }
 }
