1a1a794a37b202199085ebe03f0f51f14a3928a4
KevinEJean <158181819+KevinEJean@users.noreply.github.com>
2025-05-04 15:34:22 -0400

    Update SubscriptionController.java

--- src/main/java/backend/myfilmapp/controller/SubscriptionController.java
+++ src/main/java/backend/myfilmapp/controller/SubscriptionController.java
@@ -8,19 +8,19 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class SubscriptionController {
 
-    private final SubscriptionService service;
+    // private final SubscriptionService service;
 
-    public SubscriptionController(SubscriptionService service) {
-        this.service = service;
-    }
+    // public SubscriptionController(SubscriptionService service) {
+    //     this.service = service;
+    // }
 
-    @GetMapping("/getStatus/{username}")
-    public Boolean getStatus(@PathVariable String username) {
-        return service.getSubscriptionStatus(username);
-    }
+    // @GetMapping("/getStatus/{username}")
+    // public Boolean getStatus(@PathVariable String username) {
+    //     return service.getSubscriptionStatus(username);
+    // }
 
-    @PutMapping("/updateStatus/{username}/{status}")
-    public void updateStatus(@PathVariable String username, @PathVariable Boolean status) {
-        service.updateSubscriptionStatus(username, status);
-    }
+    // @PutMapping("/updateStatus/{username}/{status}")
+    // public void updateStatus(@PathVariable String username, @PathVariable Boolean status) {
+    //     service.updateSubscriptionStatus(username, status);
+    // }
 }
