67fcebae50406b196c34708253deb333241677a7
KevinEJean <158181819+KevinEJean@users.noreply.github.com>
2025-05-04 15:34:56 -0400

    Update SubscriptionService.java

--- src/main/java/backend/myfilmapp/service/SubscriptionService.java
+++ src/main/java/backend/myfilmapp/service/SubscriptionService.java
@@ -7,20 +7,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class SubscriptionService {
 
-    private final ClientRep rep;
+    // private final ClientRep rep;
 
-    public SubscriptionService(ClientRep rep) {
-        this.rep = rep;
-    }
+    // public SubscriptionService(ClientRep rep) {
+    //     this.rep = rep;
+    // }
 
-    public Boolean getSubscriptionStatus(String username) {
-        Client client = rep.getClientByUsername(username);
-        return client.getSubscriptionStatus();
-    }
+    // public Boolean getSubscriptionStatus(String username) {
+    //     Client client = rep.getClientByUsername(username);
+    //     return client.getSubscriptionStatus();
+    // }
 
-    public void updateSubscriptionStatus(String username, Boolean status) {
-        Client client = rep.getClientByUsername(username);
-        client.setSubscriptionStatus(status);
-        rep.save(client);
-    }
+    // public void updateSubscriptionStatus(String username, Boolean status) {
+    //     Client client = rep.getClientByUsername(username);
+    //     client.setSubscriptionStatus(status);
+    //     rep.save(client);
+    // }
 }
