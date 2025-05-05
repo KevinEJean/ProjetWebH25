c1fae66c2976da9a8e79e6f5a7d0593025d32e76
KevinEJean <158181819+KevinEJean@users.noreply.github.com>
2025-05-05 17:41:57 -0400

    Update SignUpTest.java

--- src/test/java/backend/myfilmapp/unitaire/SignUpTest.java
+++ src/test/java/backend/myfilmapp/unitaire/SignUpTest.java
@@ -68,55 +68,55 @@ public class SignUpTest {
 
 
 		
-		TimeUnit.SECONDS.sleep(3);
+		// TimeUnit.SECONDS.sleep(3);
 
 
 
 
 
-		// ouvre le site web
-		WebDriver driver = new EdgeDriver();
-		driver.get("http://localhost:80/");
+		// // ouvre le site web
+		// WebDriver driver = new EdgeDriver();
+		// driver.get("http://localhost:80/");
 		
-		// click sur l'icone profil
-		WebElement profilIcon = driver.findElement(By.name("profil-page"));
-		profilIcon.click();
+		// // click sur l'icone profil
+		// WebElement profilIcon = driver.findElement(By.name("profil-page"));
+		// profilIcon.click();
 		
-		// verifie qu'il navigue vers la page login
-		String expectedUrlLogin = "http://localhost/login";
-		assertEquals(expectedUrlLogin, driver.getCurrentUrl());
+		// // verifie qu'il navigue vers la page login
+		// String expectedUrlLogin = "http://localhost/login";
+		// assertEquals(expectedUrlLogin, driver.getCurrentUrl());
 		
-		// click sur 'Don't have an account?' (naviguer vers la page signUp)
-		WebElement toSignUp = driver.findElement(By.name("toSignUp"));
-		toSignUp.click();
+		// // click sur 'Don't have an account?' (naviguer vers la page signUp)
+		// WebElement toSignUp = driver.findElement(By.name("toSignUp"));
+		// toSignUp.click();
 		
-		// verifie qu'il navigue vers la page signUp
-		String expectedUrlSignUp = "http://localhost/signUp";
-		assertEquals(expectedUrlSignUp, driver.getCurrentUrl());
+		// // verifie qu'il navigue vers la page signUp
+		// String expectedUrlSignUp = "http://localhost/signUp";
+		// assertEquals(expectedUrlSignUp, driver.getCurrentUrl());
 		
-		WebElement usernameInput = driver.findElement(By.name("username"));
-		WebElement emailInput = driver.findElement(By.name("email"));
-		WebElement passwordInput = driver.findElement(By.name("password"));
-		WebElement passwordVerifInput = driver.findElement(By.name("passwordVerif"));
-		WebElement signUpBtn = driver.findElement(By.className("submit"));
+		// WebElement usernameInput = driver.findElement(By.name("username"));
+		// WebElement emailInput = driver.findElement(By.name("email"));
+		// WebElement passwordInput = driver.findElement(By.name("password"));
+		// WebElement passwordVerifInput = driver.findElement(By.name("passwordVerif"));
+		// WebElement signUpBtn = driver.findElement(By.className("submit"));
 
-		usernameInput.sendKeys("kevin");
-		emailInput.sendKeys("kj@mail.com");
-		passwordInput.sendKeys("secret");
-		passwordVerifInput.sendKeys("secret");
-		signUpBtn.click();
+		// usernameInput.sendKeys("kevin");
+		// emailInput.sendKeys("kj@mail.com");
+		// passwordInput.sendKeys("secret");
+		// passwordVerifInput.sendKeys("secret");
+		// signUpBtn.click();
 
-		TimeUnit.SECONDS.sleep(3);
+		// TimeUnit.SECONDS.sleep(3);
 
-		String expectedUrl = "http://localhost/profil";
-		assertEquals(expectedUrl, driver.getCurrentUrl());
+		// String expectedUrl = "http://localhost/profil";
+		// assertEquals(expectedUrl, driver.getCurrentUrl());
 		
-		// verifie que les données de l'utilisateur soit affiché
-		WebElement usernameInfo = driver.findElement(By.id("username"));
-		assertEquals("kevin", usernameInfo.getAttribute("value"));
-		WebElement passwordInfo = driver.findElement(By.id("password"));
-		assertEquals("secret", passwordInfo.getAttribute("value"));
-		WebElement emailInfo = driver.findElement(By.id("email"));
-		assertEquals("kj@mail.com", emailInfo.getAttribute("value"));
+		// // verifie que les données de l'utilisateur soit affiché
+		// WebElement usernameInfo = driver.findElement(By.id("username"));
+		// assertEquals("kevin", usernameInfo.getAttribute("value"));
+		// WebElement passwordInfo = driver.findElement(By.id("password"));
+		// assertEquals("secret", passwordInfo.getAttribute("value"));
+		// WebElement emailInfo = driver.findElement(By.id("email"));
+		// assertEquals("kj@mail.com", emailInfo.getAttribute("value"));
 	}
 }
